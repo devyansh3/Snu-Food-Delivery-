b68269208f8433e635359e6e1c6dada604e60180
Devyansh Sehgal <65131029+devyansh3@users.noreply.github.com>
2022-06-20 22:48:25 +0530

    initial project commit

---
+++ app/http/controllers/admin/statusController.js
@@ -0,0 +1,20 @@
+const Order = require('../../../models/order')
+
+function statusController() {
+    return {
+        update(req, res) {
+            Order.updateOne({_id: req.body.orderId}, { status: req.body.status }, (err, data)=> {
+                if(err) {
+                    return res.redirect('/admin/orders')
+                }
+                // Emit event 
+                //if status has been updated then redirect to orders page with updated status for that order
+                const eventEmitter = req.app.get('eventEmitter')
+                eventEmitter.emit('orderUpdated', { id: req.body.orderId, status: req.body.status })
+                return res.redirect('/admin/orders')
+            })
+        }
+    }
+}
+
+module.exports = statusController
